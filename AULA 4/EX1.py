a5bc4106401409a28c75e3a8edbbeba7785566b7
JULIA <siqueira.a@academico.ifrn.edu.br>
2026-03-26 17:53:43 +0000

    exercicio

---
+++ AULA 4/EX1.py
@@ -0,0 +1,11 @@
+#EXERCÍCIO 1
+
+a = int(input("Digite o primeiro valor: "))
+b = int(input("Digite o segundo valor: "))
+
+if a > b:
+    print("Maior =", a)
+elif b > a:
+    print("Maior =", b)
+else:
+    print("Números iguais")
