5f8b3ecec326b35209b1a8fdefa9d39121f6fd30
JULIA <siqueira.a@academico.ifrn.edu.br>
2026-03-26 17:58:09 +0000

    exercicio

--- AULA 4/EX2.py
+++ AULA 4/EX2.py
@@ -1,2 +1,17 @@
 #EXERCÍCIO 2
 
+expressao = input("Digite a operação tipo 20+100: ")
+
+if '+' in expressao:
+    n1, n2 = expressao.split('+')
+    resultado = int(n1) + int(n2)
+elif '-' in expressao:
+    n1, n2 = expressao.split('-')
+    resultado = int(n1) - int(n2)
+elif '*' in expressao:
+    n1, n2 = expressao.split('*')
+    resultado = int(n1) * int(n2)
+elif '/' in expressao:
+    n1, n2 = expressao.split('/')
+    resultado = int(n1) / int(n2)
+print("Resultado =", resultado)
