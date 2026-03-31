e8ba82c7a806c529d78ae55c39622c72fdd45f98
JULIA <siqueira.a@academico.ifrn.edu.br>
2026-03-31 16:16:03 +0000

    exercicios

--- AULA 4/EX5.py
+++ AULA 4/EX5.py
@@ -1,17 +1,18 @@
 #EXERCÍCIO 5
 
 class Pais:
-    def __init__(self, nome, populacao, area):
-        self.nome = nome
-        self.populacao = populacao
-        self.area = area
+        def __init__(self, nome, populacao, area):
+                self.nome = nome
+                        self.populacao = populacao
+                                self.area = area
 
-    def densidade(self):
-        return self.populacao / self.area
+                                    def densidade(self):
+                                            return self.populacao / self.area
 
-nome = input("Digite o nome do país: ")
-populacao = int(input("Digite a população: "))
-area = float(input("Digite a área em km²: "))
-pais = Pais(nome, populacao, area)
+                                            nome = input("Digite o nome do país: ")
+                                            populacao = int(input("Digite a população: "))
+                                            area = float(input("Digite a área em km²: "))
+                                            pais = Pais(nome, populacao, area)
 
-print(f"A densidade demográfica de {pais.nome} é {pais.densidade():.2f} hab/km²")
+                                            print(f"A densidade demográfica de {pais.nome} é {pais.densidade():.2f} hab/km²")
+                                            
