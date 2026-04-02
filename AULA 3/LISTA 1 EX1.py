00ade4ab8fdd92c5b06d270c141e4e695c66bafa
JULIA <siqueira.a@academico.ifrn.edu.br>
2026-04-02 23:58:56 +0000

    exercicios

---
+++ AULA 3/LISTA 1 EX1.py
@@ -0,0 +1,15 @@
+import math
+
+class Circulo:
+    def __init__(self, raio):
+        self.raio = raio
+    def calcular_area(self):
+        return math.pi * (self.raio ** 2)
+    def calcular_circunferencia(self):
+        return 2 * math.pi * self.raio
+    
+raio = float(input("Digite o valor do raio: "))
+c = Circulo(raio)
+
+print(f"Área do círculo: {c.calcular_area():.2f}")
+print(f"Circunferência do círculo: {c.calcular_circunferencia():.2f}")
