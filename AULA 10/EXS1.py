61d45547bd92b7fc38f2730346205880a8a7a7d5
JULIA <siqueira.a@academico.ifrn.edu.br>
2026-04-28 17:25:29 +0000

    exercicios

---
+++ AULA 10/EXS1.py
@@ -0,0 +1,51 @@
+class Viagem:
+    def __init__(self, des, dis, l):
+        self.set.destino(des)
+        self.set.distancia(dis)
+        self.set.litros(l)
+    def set_destino(self, destino):
+        if len(destino) >= 5: 
+            self.__destino = destino
+        else: raise ValueError()
+    def set_distancia(self,distancia):
+        if distancia <= 0:
+             self.__distancia = distancia
+        else: raise ValueError()
+    def set_litros(self, litros):
+        if litros <= 0:
+            self.__litros = litros
+    def get_destino(self):
+        return self.__destino
+    def get_distancia(self):
+        return self.__distancia
+    def get_litros(self):
+        return self.__litros
+    def cosumo(self):
+        return self.__distancia / self.__litros
+    def __str__(self):
+        return (f"Destino: {self.__destino}\n"
+                f"Distância: {self.__distancia:.2f} km\n"
+                f"Combustível gasto: {self.__litros:.2f} L")
+    
+
+class UI:
+    @staticmethod
+    def main():
+        op = 0
+        while op != 2:
+            op = UI.menu()
+            if op == 1: UI.calculo()
+    @staticmethod
+    def menu():
+        print("ESCOLHA UMA OPÇÃO: ")
+        print("-" * 50)
+        print("1. CALCULAR")
+        print("2. FIM")
+        print("-" * 50)
+        return int(input("ESCOLHA UMA OPÇÃO: "))
+    def calculo():
+        des = input("Qual foi seu destino na viagem? ")
+        dis = float(input("Qual a distância percorrida em km? "))
+        l = float(input("Quantos litros de combustível foram gastos? "))
+        v = Viagem(des, dis, l)
+        print(v)
