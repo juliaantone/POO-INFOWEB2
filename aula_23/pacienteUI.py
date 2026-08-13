b88e283a78e0669804c377771da54aa0f97c7103
JULIA <siqueira.a@academico.ifrn.edu.br>
2026-08-13 18:25:30 +0000

    exercicios

--- aula_23/pacienteUI.py
+++ aula_23/pacienteUI.py
@@ -7,7 +7,7 @@ class PacienteUI:
             st.header("DADOS DO PACIENTE")
             nome = st.text_input("INFORME O NOME")
             cpf = st.text_input("INFORME O CPF")
-            nasc = st.date_input("INFORME A DATA DE NASCIMENTO", min_value=date(1990, 1,1), max_value=date.today(), value=date(200, 1, 1), format=('DD/MM/YYYY'))
+            nasc = st.date_input("INFORME A DATA DE NASCIMENTO", min_value=date(1990, 1,1), max_value=date.today(), value=date(2000, 1, 1), format=('DD/MM/YYYY'))
             nasc = datetime.combine(nasc, datetime.min.time())
             fone = st.text_input("INFORME O TELEFONE")
             if st.button("IDADE"):
